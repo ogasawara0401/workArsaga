c02d24c868553b802ef461148368a8e803e6f279
yuki <y.ogasawara0401@gmail.com>
2023-07-12 14:33:32 +0900

    feat: シーダーの作成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(UserSeeder::class);
+
+        \App\Models\Post::factory(100)->create();
     }
 }
